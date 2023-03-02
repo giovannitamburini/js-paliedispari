9f62c1512df146b92ba9c971f27da5a865b735af
giovannitamburini <giovatambu@gmail.com>
2023-03-02 14:27:15 +0100

    added basic structure and pseudo-cod ex 1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+//ESERCIZIO 1
+/*Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+// PSEUDO CODICE
+/*
+- chiedi all'utente di inserire una parola e memorizza
+- trasformo la stringa inserita in array di caratteri
+- inverto l'ordine dei caratteri
+- ricompongo i caratteri invertiti in una stringa
+- devo confrontare la stringa inserita dall'utente con la striga invertita:
+  SE la stringa inserita è uguale alla stringa invertita:
+  ° scrivo nel documento che la parola risulta palindroma
+  ALTRIMENTI:
+  ° scrivo nel documento che la parola inserita non è palindroma
+  */
